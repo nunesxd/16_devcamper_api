b2040eb6ec5c30d8df625a335ac4470e70461115
Rafael Nunes <Rafael.Nunes@br.ey.com>
2021-11-12 16:24:26 -0300

    Criação dos métodos de update e delete de um curso

--- controllers/courses.js
+++ controllers/courses.js
@@ -3,6 +3,7 @@ const Bootcamp = require('../models/Bootcamp');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 
+
 // @desc    Obtem todos os cursos;
 // @route   GET /api/v1/courses;
 // @route   GET /api/v1/bootcamps/:bootcampId;
@@ -28,6 +29,7 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     res.status(200).json({ success: true, count: courses.length, data: courses });
 });
 
+
 // @desc    Obtem um curso;
 // @route   GET /api/v1/courses/:id;
 // @access  Public.
@@ -44,7 +46,8 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
     res.status(200).json({ success: true, data: course });
 });
 
-// @desc    Adiciona um curso;
+
+// @desc    Adiciona um curso (CREATE);
 // @route   POST /api/v1/bootcamp/:bootcampId/course;
 // @access  Private.
 exports.addCourse = asyncHandler(async (req, res, next) => {
@@ -60,4 +63,41 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     const course = await Course.create(req.body);
     
     res.status(200).json({ success: true, data: course });
+});
+
+
+// @desc    Atualiza um curso (UPDATE);
+// @route   PUT /api/v1/courses/id;
+// @access  Private.
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+
+    let course = await Course.findById(req.params.id);
+
+    if(!course) {
+        return next(new ErrorResponse(`Não foi possível identificar o Curso de ID num: ${req.params.id}`, 404));
+    }
+
+    course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+    
+    res.status(200).json({ success: true, data: course });
+});
+
+
+// @desc    Deleta um curso (DELETE);
+// @route   PUT /api/v1/courses/id;
+// @access  Private.
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+
+    const course = await Course.findById(req.params.id);
+
+    if(!course) {
+        return next(new ErrorResponse(`Não foi possível identificar o Curso de ID num: ${req.params.id}`, 404));
+    }
+
+    await Course.remove()
+    
+    res.status(200).json({ success: true});
 });
--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,10 @@
 const express = require('express'); 
-const {getCourses, getCourse, addCourse} = require('../controllers/courses');
+const {getCourses, getCourse, addCourse, updateCourse, deleteCourse} = require('../controllers/courses');
 // Ativamos a opção de 'mergeParams' pois estamos utlizando o re-roteamento de bootcamps para este recurso:
 const router = express.Router({ mergeParams: true });
 
 // Rotas:
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
