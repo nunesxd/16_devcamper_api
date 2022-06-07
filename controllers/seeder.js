b2b4252d33d13c58688978c7ed96b4422439cddc
Rafael Nunes <Rafael.Nunes@br.ey.com>
2022-06-07 20:58:50 -0300

    Preenchimento do campo de média dos reviews, criação do método estático (getAvgReview)

--- controllers/seeder.js
+++ controllers/seeder.js
@@ -32,8 +32,8 @@ const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
         await Course.create(courses);
-        await User.create(users);
         await Review.create(reviews);
+        await User.create(users);
         console.log('Dados importados com sucesso !');
         process.exit();
     } catch (err) {
@@ -46,8 +46,8 @@ const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
         await Course.deleteMany();
-        await User.deleteMany();
         await Review.deleteMany();
+        await User.deleteMany();
         console.log('Dados deletados com sucesso !');
         process.exit();
     } catch (err) {
